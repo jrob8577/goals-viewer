1a5d0f28b6964cd83f0eba02184d4193b0035492
jrob8577 <roberts.john@gmail.com>
2016-08-10 19:59:07 -0700

    wip: filters

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import Row from './components/row'
-
-import { TEST_DATA } from './data/issues'
+import LabelFilter from './components/label_filter'
 
 class App extends Component {
+  displayIssues() {
+    const { issues } = this.props
+    const predicate = label => {
+      const { name } = label
+
+      return name && name !== 'draft' && name.startsWith( 'team-size-' )
+    }
+
+    const issuePredicate = issue => {
+      debugger
+      return issue.labels.filter( predicate ).length > 0
+    }
+
+    return issues.filter( issuePredicate )
+  }
+
+  rows() {
+    const issues = this.displayIssues()
+
+    return issues.map( (row, index) =>
+      <Row key={`row-${index}`} {...row} />
+    )
+  }
+
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Labels</th>
-            <th>Goal</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Created</th>
-            <th>Tried</th>
-            <th>Up</th>
-            <th>Down</th>
-            <th>Check</th>
-          </tr>
-        </thead>
-        <tbody>
-          {TEST_DATA.map( (row, index) => <Row key={`row-${index}`} {...row} />)}
-        </tbody>
-      </table>
+      <div>
+        <LabelFilter labels={this.props.labels} />
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Labels</th>
+              <th>Goal</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Created</th>
+              <th>Tried</th>
+              <th>Up</th>
+              <th>Down</th>
+              <th>Check</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.rows()}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
---
+++ src/components/label_filter.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class LabelFilter extends Component {
+  render() {
+    return (
+      <div>I'm a label filter. out of draft, have a team size label</div>
+    )
+  }
+}
